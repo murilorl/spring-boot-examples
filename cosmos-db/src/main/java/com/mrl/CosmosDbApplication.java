b5eedf9c78ecb07785d9d7b74504bfeff66331d2
Murilo Rodrigues de Lima <murilorl@gmail.com>
2021-04-15 14:35:50 -0300

    Set random values to user attributes

--- cosmos-db/src/main/java/com/mrl/CosmosDbApplication.java
+++ cosmos-db/src/main/java/com/mrl/CosmosDbApplication.java
@@ -1,19 +1,20 @@
 package com.mrl;
 
-import java.util.Optional;
+import java.util.UUID;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.Assert;
 
-import reactor.core.publisher.Flux;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@Slf4j
 public class CosmosDbApplication implements CommandLineRunner {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CosmosDbApplication.class);
@@ -27,34 +28,47 @@ public class CosmosDbApplication implements CommandLineRunner {
 
 	public void run(String... var1) {
 		this.repository.deleteAll();
-		LOGGER.info("Deleted all data in container.");
+		// LOGGER.info("Deleted all data in container.");
 
-		final User testUser = new User("testId", "testFirstName", "testLastName", "test address line one");
+		User user = new User();
+		user.setId(UUID.randomUUID().toString());
+		user.setFirstName(RandomStringUtils.randomAlphabetic(6));
+		user.setLastName(RandomStringUtils.randomAlphabetic(10));
+		user.setAddress(RandomStringUtils.randomAlphanumeric(20));
 
 		// Save the User class to Azure Cosmos DB database.
-		final Mono<User> saveUserMono = repository.save(testUser);
+		final Mono<User> savedUserMono = repository.save(user);
+		log.info("Saved user with UUID {}", user.getId());
 
-		final Flux<User> firstNameUserFlux = repository.findByFirstName("testFirstName");
+		final User savedUser = savedUserMono.block();
+		log.info("Saved user: {}", savedUser);
+
+		// final Flux<User> firstNameUserFlux =
+		// repository.findByFirstName("testFirstName");
 
 		// Nothing happens until we subscribe to these Monos.
 		// findById will not return the user as user is not present.
-		final Mono<User> findByIdMono = repository.findById(testUser.getId());
-		final User findByIdUser = findByIdMono.block();
+		// final Mono<User> findByIdMono = repository.findById(testUser.getId());
+		// final User findByIdUser = findByIdMono.block();
 		// Assert.isNull(findByIdUser, "User must be null");
 
-		final User savedUser = saveUserMono.block();
-		Assert.state(savedUser != null, "Saved user must not be null");
-		Assert.state(savedUser.getFirstName().equals(testUser.getFirstName()), "Saved user first name doesn't match");
+		// final User savedUser = saveUserMono.block();
+		// Assert.state(savedUser != null, "Saved user must not be null");
+		// Assert.state(savedUser.getFirstName().equals(testUser.getFirstName()), "Saved
+		// user first name doesn't match");
 
-		firstNameUserFlux.collectList().block();
+		// firstNameUserFlux.collectList().block();
 
-		final Optional<User> optionalUserResult = repository.findById(testUser.getId()).blockOptional();
-		Assert.isTrue(optionalUserResult.isPresent(), "Cannot find user.");
+		// final Optional<User> optionalUserResult =
+		// repository.findById(testUser.getId()).blockOptional();
+		// Assert.isTrue(optionalUserResult.isPresent(), "Cannot find user.");
 
-		final User result = optionalUserResult.get();
-		Assert.state(result.getFirstName().equals(testUser.getFirstName()), "query result firstName doesn't match!");
-		Assert.state(result.getLastName().equals(testUser.getLastName()), "query result lastName doesn't match!");
+		// final User result = optionalUserResult.get();
+		// Assert.state(result.getFirstName().equals(testUser.getFirstName()), "query
+		// result firstName doesn't match!");
+		// Assert.state(result.getLastName().equals(testUser.getLastName()), "query
+		// result lastName doesn't match!");
 
-		LOGGER.info("findOne in User collection get result: {}", result.toString());
+		// LOGGER.info("findOne in User collection get result: {}", result.toString());
 	}
 }
